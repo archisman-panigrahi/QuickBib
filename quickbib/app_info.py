403160aec0a765f0011519c6ed4a9f0ebe995b8b
Archisman Panigrahi <apandada1@gmail.com>
2026-05-29 21:03:06 -0400

    update version

--- quickbib/app_info.py
+++ quickbib/app_info.py
@@ -3,7 +3,7 @@ import sys
 
 # Application metadata
 APP_NAME = "QuickBib"
-APP_VERSION = "0.9.0"
+APP_VERSION = "0.9.1"
 HOMEPAGE = "https://archisman-panigrahi.github.io/QuickBib/"
 REPO_URL = "https://github.com/archisman-panigrahi/QuickBib"
 WEBAPP_URL = "https://quickbib.streamlit.app/"
--- windows_packaging/quickbib.nsi
+++ windows_packaging/quickbib.nsi
@@ -3,7 +3,7 @@
 
 !define APP_NAME "QuickBib"
 !define COMPANY "Archisman Panigrahi"
-!define VERSION "0.9.0"
+!define VERSION "0.9.1"
 
 ; Installer display name shown in the window title and installer UI
 Name "${APP_NAME}"
