6600007606ea6337435444ff75c920fdf44845c7
Archisman Panigrahi <apandada1@gmail.com>
2026-06-05 16:23:16 -0400

    update version

--- quickbib/app_info.py
+++ quickbib/app_info.py
@@ -3,7 +3,7 @@ import sys
 
 # Application metadata
 APP_NAME = "QuickBib"
-APP_VERSION = "0.9.1"
+APP_VERSION = "0.9.2"
 HOMEPAGE = "https://archisman-panigrahi.github.io/QuickBib/"
 REPO_URL = "https://github.com/archisman-panigrahi/QuickBib"
 WEBAPP_URL = "https://github.com/archisman-panigrahi/QuickBib/webapp/"
