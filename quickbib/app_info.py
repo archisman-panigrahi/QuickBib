df110e29c54e5b633915186a2f611ab6c64bcb1d
Archisman Panigrahi <apandada1@gmail.com>
2026-06-13 01:17:32 -0400

    Fix webapp URL

--- quickbib/app_info.py
+++ quickbib/app_info.py
@@ -6,7 +6,7 @@ APP_NAME = "QuickBib"
 APP_VERSION = "0.9.3"
 HOMEPAGE = "https://archisman-panigrahi.github.io/QuickBib/"
 REPO_URL = "https://github.com/archisman-panigrahi/QuickBib"
-WEBAPP_URL = "https://github.com/archisman-panigrahi/QuickBib/webapp/"
+WEBAPP_URL = "https://archisman-panigrahi.github.io/QuickBib/webapp"
 ISSUES_URL = "https://github.com/archisman-panigrahi/QuickBib/issues"
 ALGORITHM_VISUALS_URL = "https://github.com/archisman-panigrahi/doi2bib3/blob/main/docs/ALGORITHM_VISUALS.md#2-identifier-resolution-decision-tree"
 # LICENSE is located in the repository root (one level up from the package dir)
